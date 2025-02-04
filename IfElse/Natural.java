0c7ebeb391ee5f992e2db7e8996b0f59ca5f4bf8
AbhinavGalar <abhinavgalar442@gmail.com>
2025-02-04 22:53:07 +0530

    feb4

---
+++ IfElse/Natural.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Natural {
+  public static void main(String[] args) {
+    System.out.println("enter your number");
+    Scanner obj=new Scanner(System.in);
+    int num;
+    num=obj.nextInt();
+    if ()
+  }  
+}

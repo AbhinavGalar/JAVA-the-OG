79514e662a4a034a68d86e3b02e3bbc3a3556399
AbhinavGalar <abhinavgalar442@gmail.com>
2025-02-16 23:52:26 +0530

    p

---
+++ IfElse/Switch/Loops/Array/Practice.java
@@ -0,0 +1,26 @@
+// import java.util.Scanner;
+
+// public class Practice{
+// public static void main(String[] args) {
+//     Scanner obj=new Scanner(System.in);
+
+//     int arr[]=new int []{1,2,3,4,5};
+//     int sum=0;
+
+//     for(int i=1;i<=arr.length;++i){
+//         sum=sum+i;
+//         System.out.println(sum);
+//     }
+// }
+// }
+
+
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner (System.in);
+
+        int arr
+    }
+}

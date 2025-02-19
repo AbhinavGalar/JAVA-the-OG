05cd74740138a5809771709fbc5868674feac424
AbhinavGalar <abhinavgalar442@gmail.com>
2025-02-19 23:53:20 +0530

    sorting of array

---
+++ IfElse/Switch/Loops/Array/Maxmin.java
@@ -0,0 +1,28 @@
+// import java.util.Scanner;
+
+// public class Maxmin {
+//     public static void main(String[] args) {
+//          int arr[]= new int[]{1,2,30,6,69,100,400,1000};
+
+//           int max=arr[0];
+//          for (int i=0;i<arr.length;++i){
+//          if(arr[i]>max){
+//             max=arr[i];
+//          }
+           
+//          }
+//          System.out.println(max);
+
+//     }
+
+// }
+
+// maximum minimum 
+
+// max(arr[i]>max)
+// min (arr[i]<min)
+
+
+
+
+

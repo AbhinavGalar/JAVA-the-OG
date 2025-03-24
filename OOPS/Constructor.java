1906d5da588078d452c4be8d2f96ec7a1800740a
AbhinavGalar <abhinavgalar442@gmail.com>
2025-03-24 23:24:10 +0530

    oops

---
+++ OOPS/Constructor.java
@@ -0,0 +1,30 @@
+// import java.util.Scanner;
+// public class Constructor {
+
+//      int a;
+//      void display(){
+//         System.out.println("hello");
+//      } 
+     
+       
+//      public static void main(String[] args) {
+//         Constructor obj= new Constructor();
+//         obj.display();
+   
+//      }
+// }
+
+import java.util.Scanner;
+public class Constructor {
+   int a ;
+     void display(){
+      System.out.println("hello");
+
+     
+     }
+    
+     public static void main(String[] args) {
+      Constructor obj= new Constructor();
+      obj.display();
+     }
+}

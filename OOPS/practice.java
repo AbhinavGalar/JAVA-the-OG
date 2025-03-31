f18a5135d620db432b5c1577d992e91d5f34e69d
AbhinavGalar <abhinavgalar442@gmail.com>
2025-03-31 23:42:30 +0530

    p

--- OOPS/practice.java
+++ OOPS/practice.java
@@ -25,27 +25,54 @@
 // }
 
 
- class Student {
+//  class Student {
+//     String name;
+//     int age;
+//     int marks;
+
+//     public void data(){
+//         System.out.println(this.age);
+//         System.out.println(this.marks);
+//         System.out.println(this.name);
+
+//     }
+//  }
+// public class practice {
+// public static void main(String[] args) {
+//     Student s1= new Student();
+
+//     s1.name="drashti";
+//     s1.age=19;
+//     s1.marks=28;
+
+//     s1.data();
+// }
+    
+// }
+
+import java.util.Scanner;
+
+class intro{
     String name;
     int age;
-    int marks;
+    int price;
 
-    public void data(){
-        System.out.println(this.age);
-        System.out.println(this.marks);
-        System.out.println(this.name);
 
-    }
- }
-public class practice {
-public static void main(String[] args) {
-    Student s1= new Student();
+public void data(){
+    System.out.println(this.age);
+    System.out.println(this.name);
+    System.out.println(this.price);
 
-    s1.name="drashti";
-    s1.age=19;
-    s1.marks=28;
-
-    s1.data();
 }
-    
+
+}
+public class practice {
+
+    public static void main(String[] args) {
+        intro i1 =new intro();
+        i1.age=19;
+        i1.name="abhinav";
+        i1.price=1;
+        i1.data();
+    }
 }
